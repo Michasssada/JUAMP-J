1ae2c3ca1a1c62f550ade0f2867c2a7644a4d766
Michasssada <michaslate@gmail.com>
2025-01-09 22:10:50 +0100

    tak

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # JUAMP-J
 JUAMP ale w java. LUBISZ BUGI? JA TEŻ!!!!!!! DLA TEGO JUAMP-J JEST DLA CIEBIE SKUBAŃCU.
+# czym się to różni od innych JUAMPów
+* polityka
+* związki
+* można być dyktatorem
+* można być napadniętym przez sebixa
+  
+(te funkcje nie są jeszcze wprowadzone)
 # inspiracja
 [JUAMP](https://github.com/gorciu-official/JUAMP)
--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,25 @@
 import  java.util.*;
 public class Main {
     Scanner scanner = new Scanner(System.in);
+    color Color = new color();
     public static void main(String[] args) {
         Main m = new Main();
         m.rMain();
     }
     public void rMain(){//real main
-        String nameSurname = scanner.nextLine();
-        System.out.println(nameSurname);
+        System.out.println(Color.GREEN+"niżer");
+        while(true){
+            String nameSurname = scanner.nextLine();
+            System.out.println(nameSurname);
+
+        }
     }
+
+}
+class color{
+    final String RESET = "\u001B[0m";
+    final String RED = "\u001B[31m";
+    final String GREEN = "\u001B[32m";
+    final String YELLOW = "\u001B[33m";
+    final String BLUE = "\u001B[34m";
 }
