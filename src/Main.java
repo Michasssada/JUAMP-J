341b4fdbb6a5e512ae3bb7e854fbe0ad3ee4d80b
Michasssada <michaslate@gmail.com>
2025-01-09 21:58:02 +0100

    da

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 //o ty huju bobże
+import  java.util.*;
 public class Main {
+    Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-
+        Main m = new Main();
+        m.rMain();
+    }
+    public void rMain(){//real main
+        String nameSurname = scanner.nextLine();
+        System.out.println(nameSurname);
     }
 }
